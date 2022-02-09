b2abfa97903214c53c33b1007ff25be699e3f798
CPONS <cponsdev@gmail.com>
2022-02-09 20:26:54 +0100

    Ajout de la navbar + footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    /**
+     *@Route("/", name="home")
+     */
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
