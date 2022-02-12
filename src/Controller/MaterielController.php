a2c55d4f86c48f54f2f817c9b70bebd17145c22b
CPONS <cponsdev@gmail.com>
2022-02-12 21:44:06 +0100

    template fini

---
+++ src/Controller/MaterielController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MaterielController extends AbstractController
+{
+    /**
+     *@Route("/materiel", name="materiel")
+     */
+    public function index(): Response
+    {
+        return $this->render('materiel/index.html.twig', [
+            'controller_name' => 'MaterielController',
+        ]);
+    }
+}
